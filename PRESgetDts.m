d3feaa5fea3651f7e1835292b58f4c719e17a6d3
wangshanshan <42035727+wangshanshanCQU@users.noreply.github.com>
2019-11-09 12:39:13 +0800

    Delete PRESgetDts.m

--- PRESgetDts.m
+++
@@ -1,6 +0,0 @@
-function Dts = PRESgetDts(Xs,Xt,Z)
-[d,Nt] = size(Xt);
-[d,Ns] = size(Xs);
-% Dts_mmd =opts.beta* norm((sum(Xt,2)./Nt-sum(Xs,2)./Ns).^2,2)^2;
-% Dts = norm(Xs*Z-Xt,'fro')^2+Dts_mmd;
-Dts = norm(Xs*Z-Xt,'fro')^2;
